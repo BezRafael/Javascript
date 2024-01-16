1f64a81e52c1a9d2a80efbfe1d1ad21304baf82e
Rafael Bezerra <155703891+BezRafael@users.noreply.github.com>
2024-01-16 15:46:43 -0300

    Add files via upload

---
+++ ConversorTemperatura/ConversorTemperatura.js
@@ -0,0 +1,9 @@
+function CparaF(){
+    var C = Number (document.getElementById("entC").value);
+    window.alert((C*9/5) + 32 + '°F');
+}
+
+function FparaC(){
+    var F = Number (document.getElementById("entF").value);
+    window.alert((F-32)*5/9 + '°C');
+}
